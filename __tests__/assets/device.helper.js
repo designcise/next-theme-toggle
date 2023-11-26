2a60c5d035834eab7ca0b9803860c0391cb29935
designcise <dev@designcise.com>
2023-11-26 16:57:06 +0100

    chore: added gitignore

---
+++ __tests__/assets/device.helper.js
@@ -0,0 +1,20 @@
+export function setDeviceTheme(theme) {
+    Object.defineProperty(window, 'matchMedia', {
+        writable: true,
+        value: jest.fn().mockImplementation(query => ({
+            matches: theme === 'dark',
+            media: query,
+            onchange: null,
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+        }))
+    })
+}
+
+export function setDeviceCookie(name, value) {
+    Object.defineProperty(window.document, 'cookie', {
+        writable: true,
+        value: `${name}=${value}`,
+    });
+}
