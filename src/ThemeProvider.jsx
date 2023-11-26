3d70190291c8551a81ae4a8e7c64f1a726b92447
designcise <dev@designcise.com>
2023-11-26 01:04:50 +0100

    feat: inject anti-flicker script to dom

--- src/ThemeProvider.jsx
+++ src/ThemeProvider.jsx
@@ -1,10 +1,15 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, memo } from 'react';
 import ThemeContext from './ThemeContext';
 import { getPreference, setPreference, getColors } from './helper/theme.helper';
 
 const color = getColors();
+
+const AntiFlickerScript = memo(({ theme, color }) => {
+    const scriptSrc = (() => `(function (theme,root) {root.classList.remove(\`'${Object.values(color).join("','")}'\`);root.classList.add(theme);root.style.colorScheme = theme;})('${theme}', document.firstElementChild)`)();
+    return <script dangerouslySetInnerHTML={{ __html: scriptSrc }} />
+}, () => true);
 
 export default function ThemeProvider({
     children,
@@ -19,6 +24,7 @@ export default function ThemeProvider({
 
     return (
         <ThemeContext.Provider value={{ theme, color, setTheme }}>
+            <AntiFlickerScript theme={theme} color={color} />
             {children}
         </ThemeContext.Provider>
     );
