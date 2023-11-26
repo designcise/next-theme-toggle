cedc04c5d8b960484d0c462eec252dd282e3b849
designcise <dev@designcise.com>
2023-11-26 20:49:55 +0100

    refactor: use alterantive syntax for iife

--- src/AntiFlickerScript.jsx
+++ src/AntiFlickerScript.jsx
@@ -4,6 +4,6 @@ export default memo(function AntiFlickerScript({ theme, color }) {
     const colorValues = Object.values(color).map(value => String(value));
     const classList = colorValues.join("','");
 
-    const script = `(function(theme,root){root.classList.remove('${classList}');root.classList.add(theme);root.style.colorScheme=theme;})('${theme}',document.firstElementChild)`;
+    const script = `!function(theme,root){root.classList.remove('${classList}');root.classList.add(theme);root.style.colorScheme=theme;}('${theme}',document.firstElementChild)`;
     return <script dangerouslySetInnerHTML={{ __html: script }} />
 }, () => true);
