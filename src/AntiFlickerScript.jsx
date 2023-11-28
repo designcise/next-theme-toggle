1eea9f430df27c1a17e066bbe272bdec69c85459
designcise <dev@designcise.com>
2023-11-28 20:51:05 +0100

    feat: export getColors() function

--- src/AntiFlickerScript.jsx
+++
@@ -1,9 +0,0 @@
-import React, { memo } from 'react';
-
-export default memo(function AntiFlickerScript({ theme, color }) {
-    const colorValues = Object.values(color).map(value => String(value));
-    const classList = colorValues.join("','");
-
-    const script = `!function(theme,root){root.classList.remove('${classList}');root.classList.add(theme);root.style.colorScheme=theme;}('${theme}',document.firstElementChild)`;
-    return <script dangerouslySetInnerHTML={{ __html: script }} />
-}, () => true);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as ThemeProvider } from './ThemeProvider.jsx';
 export { default as useTheme } from './hook/useTheme';
+export { getColors } from './helper/theme.helper';
