6ce2fc2e728698808d6ddf1af64c4d6abb6be589
designcise <dev@designcise.com>
2023-11-30 21:50:59 +0100

    feat: use local storage instead of cookies

--- src/component/AntiFlickerScript.jsx
+++ src/component/AntiFlickerScript.jsx
@@ -1,9 +1,12 @@
 import React, { memo } from 'react';
 
-export default memo(function AntiFlickerScript({ theme, color }) {
+export default memo(function AntiFlickerScript({ storageKey, defaultTheme, color }) {
     const classList = Object.values(color).join("','");
-    const script = '(function(theme,root){'
+    const preferredTheme = `localStorage.getItem('${storageKey}')`;
+    const fallbackTheme = defaultTheme ? `'${defaultTheme}'` : `(window.matchMedia('(prefers-color-scheme: ${color.dark})').matches ? '${color.dark}' : '${color.light}')`;
+    const script = '(function(root){'
+        + `const theme=${preferredTheme}??${fallbackTheme};`
         + `root.classList.remove('${classList}');root.classList.add(theme);root.style.colorScheme=theme;`
-     + `})('${theme}',document.firstElementChild)`;
+     + `})(document.documentElement)`;
     return <script dangerouslySetInnerHTML={{ __html: script }} />
 }, () => true);
