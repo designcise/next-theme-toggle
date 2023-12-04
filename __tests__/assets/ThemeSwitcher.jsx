3c70f3eda200cdfada0013ba01df816fb41f5a48
designcise <dev@designcise.com>
2023-12-04 04:01:12 +0100

    test: updated to use themes instead of colors object

--- __tests__/assets/ThemeSwitcher.jsx
+++ __tests__/assets/ThemeSwitcher.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useTheme } from '../../src/client';
+import React from 'react'
+import { useTheme } from '../../src/client'
 
 export default function ThemeSwitcher() {
-    const { theme, color, setTheme } = useTheme();
+  const { theme, themes, color, setTheme } = useTheme()
 
-    return (
-        <>
-            <div>Active Theme: {theme}</div>
-            <button onClick={() => setTheme(color.light)}>Light Theme</button>
-            <button onClick={() => setTheme(color.dark)}>Dark Theme</button>
-        </>
-    )
+  return (
+    <>
+      <div>Active Theme: {theme}</div>
+      <div>Active Color: {color}</div>
+      <button onClick={() => setTheme(themes.light)}>Light Theme</button>
+      <button onClick={() => setTheme(themes.dark)}>Dark Theme</button>
+      <button onClick={() => setTheme(themes.auto)}>Auto Theme</button>
+    </>
+  )
 }
