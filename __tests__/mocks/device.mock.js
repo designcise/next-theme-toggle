446ad58adc8b2d048bdf237c28cad482d4e201c0
designcise <dev@designcise.com>
2023-12-04 03:58:34 +0100

    test: moved to mocks folder + split into individual files for unit tests

--- __tests__/mocks/device.mock.js
+++ __tests__/mocks/device.mock.js
@@ -1,50 +1,55 @@
-Object.defineProperty(window, 'matchMedia', {
+export function mockMatchMedia() {
+  Object.defineProperty(window, 'matchMedia', {
     writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // Deprecated
-        removeListener: jest.fn(), // Deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
     })),
-});
+  })
+}
 
-const localStorageMock = (function() {
+export function mockPreferredColorScheme(theme, options = {}) {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: theme === 'dark',
+      media: query,
+      onchange: null,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+      ...options,
+    })),
+  })
+}
+
+export function mockLocalStorage() {
+  const localStorageMock = (function () {
     let store = {}
 
     return {
-        getItem: function(key) {
-            return store[key] || null;
-        },
-        setItem: function(key, value) {
-            store[key] = value.toString();
-        },
-        removeItem: function(key) {
-            delete store[key];
-        },
-        clear: function() {
-            store = {};
-        },
-    };
-})();
+      getItem: function (key) {
+        return store[key] || null
+      },
+      setItem: function (key, value) {
+        store[key] = value.toString()
+      },
+      removeItem: function (key) {
+        delete store[key]
+      },
+      clear: function () {
+        store = {}
+      },
+    }
+  })()
 
-Object.defineProperty(window, 'localStorage', {
+  Object.defineProperty(window, 'localStorage', {
     value: localStorageMock,
-});
-
-export function mockPreferredColorScheme(theme) {
-    Object.defineProperty(window, 'matchMedia', {
-        writable: true,
-        value: jest.fn().mockImplementation(query => ({
-            matches: theme === 'dark',
-            media: query,
-            onchange: null,
-            addEventListener: jest.fn(),
-            removeEventListener: jest.fn(),
-            dispatchEvent: jest.fn(),
-        }))
-    })
+  })
 }
---
+++ __tests__/mocks/localStorage.mock.js
@@ -0,0 +1,3 @@
+import { mockLocalStorage } from './device.mock'
+
+mockLocalStorage()
