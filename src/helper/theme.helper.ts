67964c942bd1c8bb6e01025627c578bcb53fa2b5
designcise <dev@designcise.com>
2024-01-05 02:49:53 +0100

    refactor: add types + create theme object + rename methods

--- src/helper/theme.helper.ts
+++ src/helper/theme.helper.ts
@@ -1,41 +1,63 @@
 import { read, write, erase } from '../adapter/storage.adapter'
 import { isServer } from './env.helper'
+import { Theme, Themes, ThemeKeys, ThemeInput, Color, Colors } from '../types'
 
-export const themes = { dark: 'dark', light: 'light', auto: 'auto' }
-export const colors = {
-  [themes.dark]: 'dark',
-  [themes.light]: 'light',
-  get [themes.auto]() {
-    return isServer()
-      ? undefined
-      : window.matchMedia(`(prefers-color-scheme: ${this[themes.dark]})`).matches
-        ? this[themes.dark]
-        : this[themes.light]
+export const colors: Colors = { dark: 'dark', light: 'light' }
+
+export const themes: Themes = {
+  light: { type: 'light', color: colors.light },
+  dark: { type: 'dark', color: colors.dark },
+  auto: {
+    type: 'auto',
+    get color() {
+      return isServer()
+        ? undefined
+        : window.matchMedia(`(prefers-color-scheme: ${themes.dark.color})`).matches
+          ? themes.dark.color
+          : themes.light.color
+    },
   },
 }
 
-export const getTheme = (storageKey, defaultTheme) =>
-  isServer() ? undefined : read(storageKey) ?? defaultTheme ?? themes.auto
+export const getThemeByKey = (storageKey: string, defaultTheme?: ThemeInput): Theme => {
+  if (isServer()) return themes.dark
 
-export const getColorByTheme = (theme) => colors[theme ?? themes.auto]
+  const themeKey = read(storageKey) ?? defaultTheme
 
-const colorToTheme = {
-  [colors.dark]: themes.dark,
-  [colors.light]: themes.light,
+  if (typeof themeKey === 'undefined') {
+    return themes.auto as Theme
+  }
+
+  return themes[themeKey] as Theme
 }
 
-export const flipThemeByColor = (color) =>
-  colorToTheme[color] === themes.dark ? themes.light : themes.dark
+const findThemeByProp = <T extends ThemeKeys>(prop: T, value: Theme[T]): Theme | null => {
+  for (const [, theme] of Object.entries(themes)) {
+    if (theme[prop] === value) {
+      return theme
+    }
+  }
 
-const applyTheme = (theme) => {
-  const color = getColorByTheme(theme)
-  const root = document.firstElementChild
+  return null
+}
+
+export const getColorByThemeType = (themeType?: Theme['type']): Color =>
+  findThemeByProp('type', themeType ?? 'auto')?.color as Color
+
+export const getThemeByColor = (color: Color): Theme | null => findThemeByProp('color', color)
+
+export const getFlippedThemeByColor = (color: Color): Theme =>
+  getThemeByColor(color)?.color === themes.dark.color ? themes.light : themes.dark
+
+const applyTheme = (theme: ThemeInput) => {
+  const color = getColorByThemeType(theme)
+  const root = document.firstElementChild as HTMLHtmlElement
   root.classList.remove(...Object.values(colors))
   root.classList.add(color)
   root.style.colorScheme = color
 }
 
-export const saveTheme = (storageKey, theme) => {
+export const saveTheme = (storageKey: string, theme: ThemeInput) => {
   erase(storageKey)
   write(storageKey, theme)
   applyTheme(theme)
