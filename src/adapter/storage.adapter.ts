8a499dc77c109be9e62949e20a7c1e7604dabe07
designcise <dev@designcise.com>
2024-01-05 02:50:58 +0100

    feat: add types

--- src/adapter/storage.adapter.ts
+++ src/adapter/storage.adapter.ts
@@ -1,13 +1,9 @@
-export const read = (key) => localStorage.getItem(key)
+import { Theme } from '../types'
 
-export const write = (key, value) => {
-  localStorage.setItem(key, value)
-}
+export const read = (key: string) => localStorage.getItem(key) as Theme['type'] | null
 
-export const erase = (key) => {
-  localStorage.removeItem(key)
-}
+export const write = (key: string, value: Theme['type']) => localStorage.setItem(key, value)
 
-export const clear = () => {
-  localStorage.clear()
-}
+export const erase = (key: string) => localStorage.removeItem(key)
+
+export const clear = () => localStorage.clear()
