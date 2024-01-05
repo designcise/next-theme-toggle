66a9ab0e2910af74594f715bb94983be7d51819c
designcise <dev@designcise.com>
2024-01-05 02:56:08 +0100

    refactor: use theme and themes object instead of color

--- __tests__/assets/ThemeManualToggle.tsx
+++ __tests__/assets/ThemeManualToggle.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 import { useTheme } from '../../src/client'
 
 export default function ToggleThemeButton() {
-  const { theme, themes, colors, setTheme } = useTheme()
+  const { theme, themes, setTheme } = useTheme()
 
   return (
-    <button onClick={() => setTheme(theme === themes.dark ? colors.light : colors.dark)}>
+    <button onClick={() => setTheme(theme.type === themes.dark.type ? themes.light : themes.dark)}>
       Toggle Theme
     </button>
   )
