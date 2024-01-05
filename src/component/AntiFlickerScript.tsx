6fe49ca3b1572222daa38411678ab5fcf2956b9b
designcise <dev@designcise.com>
2024-01-05 02:54:24 +0100

    refactor: use updated themes and colors + add types

--- src/component/AntiFlickerScript.tsx
+++ src/component/AntiFlickerScript.tsx
@@ -1,16 +1,16 @@
 import React, { memo } from 'react'
-import { themes, colors as palette, getColorByTheme } from '../helper/theme.helper'
+import { themes, colors, getColorByThemeType } from '../helper/theme.helper'
+import { AntiFlickerScriptProps } from '../types'
 
 export default memo(
-  function AntiFlickerScript({ storageKey, defaultTheme }) {
-    const { [themes.auto]: _, ...colors } = palette
-    const classList = Object.values(colors).join("','")
+  function AntiFlickerScript({ storageKey, defaultTheme }: AntiFlickerScriptProps) {
+    const classList = Object.values(colors).sort().join("','")
     const preferredTheme = `localStorage.getItem('${storageKey}')`
     const fallbackTheme = `(window.matchMedia('(prefers-color-scheme: ${colors.dark})').matches?'${colors.dark}':'${colors.light}')`
     const script =
       '(function(root){' +
-      `const pref=${preferredTheme}??'${getColorByTheme(defaultTheme)}';` +
-      `const theme=(pref&&pref!=='${themes.auto}')?pref:${fallbackTheme};` +
+      `const pref=${preferredTheme}??'${getColorByThemeType(defaultTheme)}';` +
+      `const theme=(pref&&pref!=='${themes.auto.type}')?pref:${fallbackTheme};` +
       `root.classList.remove('${classList}');root.classList.add(theme);root.style.colorScheme=theme;` +
       `})(document.documentElement)`
     return <script dangerouslySetInnerHTML={{ __html: script }} />
